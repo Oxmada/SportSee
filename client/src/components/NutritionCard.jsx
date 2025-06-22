ba1740c9371d02c04074742e4337eb2166c0828d
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-06-22 21:09:36 +0300

    Réalisation du component NutritionCard.jsx

--- client/src/components/NutritionCard.jsx
+++ client/src/components/NutritionCard.jsx
@@ -1,6 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../services/api';
+import energyIcon from '../assets/energy.svg';
+import chickenIcon from '../assets/chicken.svg';
+import appleIcon from '../assets/apple.svg';
+import cheeseburgerIcon from '../assets/cheeseburger.svg';
+
 function NutritionCard() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const user = await getUser(12);
+                setData(user);
+            } catch (error) {
+                console.error('Erreur chargement données nutritionnelles :', error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    if (!data) return null;
+
     return (
-        <div>
+        <div className="nutritionCards">
+            <div className="card red">
+                <div className='icon-container'>
+                    <img src={energyIcon} alt="flamme" />
+                </div>
+                <div className='text-container'>
+                    <p className='value'>{data.calorie}kCal</p>
+                    <span className='label'>Calories</span>
+                </div>
+            </div>
+            <div className="card blue">
+                <div className='icon-container'>
+                    <img src={chickenIcon} alt="Protéines" />
+                </div>
+                <div className='text-container'>
+                    <p className='value'>{data.protein}g</p>
+                    <span className='label'>Proteines</span>
+                </div>
+            </div>
+            <div className="card yellow">
+                <div className='icon-container'>
+                    <img src={appleIcon} alt="Glucides" />
+                </div>
+                <div className='text-container'>
+                    <p className='value'>{data.carbohydrate}g</p>
+                    <span className='label'>Glucides</span>
+                </div>
+            </div>
+            <div className="card pink">
+                <div className='icon-container'>
+                    <img src={cheeseburgerIcon} alt="Lipides" />
+                </div>
+                <div className='text-container'>
+                    <p className='value'>{data.lipid}g</p>
+                    <span className='label'>Lipides</span>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/pages/Profil.jsx
+++ client/src/pages/Profil.jsx
@@ -33,6 +33,7 @@ function Profil() {
             <AverageSessionChart />
             <PerformanceRadarChart />
             <ScoreRadialChart />
+            <NutritionCard />
 
         </div>
     );
